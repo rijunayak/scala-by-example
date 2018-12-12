39daff8d0a84b2636a8b6bf1538c5019aa370e44
Riju Nayak <riju.nayak@gmail.com>
2018-12-12 23:01:08 +0530

    Add quick sort

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,5 +1,13 @@
 package example
 
+import Utilities.QuickSort
+
 object Hello extends App {
+  val array = Array(5,4,3,2,1)
+  val sortedArray = new QuickSort().sort(array)
   println("Hello")
+  print(s"Sorted Array - ")
+  sortedArray.foreach(element => {
+    print(s"$element ")
+  })
 }
